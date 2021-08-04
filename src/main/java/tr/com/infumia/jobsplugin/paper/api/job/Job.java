51fb8c890c348f2b566ef25d70ca698e14098bcf
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-04 15:32:25 +0300

    condition, mission, reward.

--- src/main/java/tr/com/infumia/jobsplugin/paper/api/job/Job.java
+++ src/main/java/tr/com/infumia/jobsplugin/paper/api/job/Job.java
@@ -1,11 +1,20 @@
 package tr.com.infumia.jobsplugin.paper.api.job;
 
+import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
+import tr.com.infumia.jobsplugin.paper.api.mission.Mission;
 
 /**
  * an interface to determine jobs.
  */
 public interface Job {
+
+  /**
+   * adds the mission.
+   *
+   * @param mission the mission to add.
+   */
+  void addMission(@NotNull Mission mission);
 
   /**
    * obtains the id.
@@ -14,4 +23,21 @@ public interface Job {
    */
   @NotNull
   String getId();
+
+  /**
+   * gets the mission.
+   *
+   * @param id the id to get.
+   *
+   * @return condition.
+   */
+  @NotNull
+  Optional<Mission> getMission(@NotNull String id);
+
+  /**
+   * removes the mission.
+   *
+   * @param id the id to remove.
+   */
+  void removeMission(@NotNull String id);
 }
