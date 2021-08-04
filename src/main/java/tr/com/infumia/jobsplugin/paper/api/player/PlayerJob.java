d3d2d2cb789143720a3b3074a7f853424c91c160
Hasan Demirtaş <utsukushihito@outlook.com>
2021-08-04 15:22:01 +0300

    Update PlayerJob.java

--- src/main/java/tr/com/infumia/jobsplugin/paper/api/player/PlayerJob.java
+++ src/main/java/tr/com/infumia/jobsplugin/paper/api/player/PlayerJob.java
@@ -1,8 +1,42 @@
 package tr.com.infumia.jobsplugin.paper.api.player;
 
+import java.util.Optional;
+import java.util.UUID;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * an interface to determine player jobs.
  */
 public interface PlayerJob {
 
+  /**
+   * obtains the offline player.
+   *
+   * @return player as offline player.
+   */
+  @NotNull
+  default OfflinePlayer getOfflinePlayer() {
+    return Bukkit.getOfflinePlayer(this.getPlayerUniqueId());
+  }
+
+  /**
+   * obtains the player.
+   *
+   * @return player.
+   */
+  @NotNull
+  default Optional<Player> getPlayer() {
+    return Optional.ofNullable(Bukkit.getPlayer(this.getPlayerUniqueId()));
+  }
+
+  /**
+   * obtains the player unique id.
+   *
+   * @return player's unique id.
+   */
+  @NotNull
+  UUID getPlayerUniqueId();
 }
